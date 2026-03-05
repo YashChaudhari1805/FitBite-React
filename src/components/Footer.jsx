9dc0f03954248723fb16b3b5e0f9b14ef16d26fe
YK Chaudhari <155022241+YashChaudhari1805@users.noreply.github.com>
2026-03-05 19:57:27 +0530

    Minor changes to header and creation of Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,36 @@ import React from "react";
 function Footer() {
     return (
         <>
-            
+            <div className="p-4 font-['Candara']">
+                <footer className="footer sm:footer-horizontal text-base-content p-10 bg-blue-200 rounded-box mx-2">
+                    <aside>
+                        <img className='w-30 h-30' src='./vite.svg' />
+                        <p>
+                            FitBite
+                            <br />
+                            Empowering Healthy Lifestyle since 2026
+                        </p>
+                    </aside>
+                    <nav>
+                        <h6 className="footer-title">Services</h6>
+                        <a className="link link-hover">Recipes</a>
+                        <a className="link link-hover">Workouts</a>
+                        <a className="link link-hover">Diet Tracking</a>
+                    </nav>
+                    <nav>
+                        <h6 className="footer-title">Company</h6>
+                        <a className="link link-hover">About us</a>
+                        <a className="link link-hover">Contact</a>
+                        <a className="link link-hover">Jobs</a>
+                    </nav>
+                    <nav>
+                        <h6 className="footer-title">Legal</h6>
+                        <a className="link link-hover">Terms of use</a>
+                        <a className="link link-hover">Privacy policy</a>
+                        <a className="link link-hover">Cookie policy</a>
+                    </nav>
+                </footer>
+            </div>
         </>
     )
 }
